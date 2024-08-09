d1d27b045728f4102800126c4501fd74f1ca2d77
Tushar Maurya <129389074+Gearz9@users.noreply.github.com>
2024-08-09 13:07:19 +0530

    Final Code

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,21 +1,26 @@
 from EmotionDetection.emotion_detection import emotion_detector
-
+from EmotionDetection.emotion_detection import emotion_predictor
 import unittest
-class TestEmotionDetection(unittest.TestCase):
-    def test_emotion_predictor(self):
-        result_1 = emotion_predictor(emotion_detector("I am glad this happened"))
-        self.assertEqual(result_1['dominant_emotion'], 'joy')
 
-        result_2 = emotion_predictor(emotion_detector("I am really mad about this"))
-        self.assertEqual(result_2['dominant_emotion'], 'anger')
 
-        result_3 = emotion_predictor(emotion_detector("I feel disgusted just hearing about this"))
-        self.assertEqual(result_3['dominant_emotion'], 'disgust')
+class TestEmotionDetector(unittest.TestCase):
+    def test_emotion_detector(self):        
+        # Test case for joy emotion
+        result_1 = emotion_detector('I am glad this happened')
+        self.assertEqual(result_1['Dominant'], 'joy')
         
-        result_4 = emotion_predictor(emotion_detector("I am so sad about this"))
-        self.assertEqual(result_4['dominant_emotion'], 'sadness')
+        # Test case for anger emotion
+        result_2 = emotion_detector('I am really mad about this')
+        self.assertEqual(result_2['Dominant'], 'anger')
+        
+        # Test case for disgust emotion
+        result_3 = emotion_detector('I feel disgusted just hearing about this')
+        self.assertEqual(result_3['Dominant'], 'disgust')
 
-        result_5 = emotion_predictor(emotion_detector("I am really afraid that this will happen"))
-        self.assertEqual(result_5['dominant_emotion'], 'fear')
+        result_4 = emotion_detector('I so sad about this')
+        self.assertEqual(result_4['Dominant'], 'sadness')
+
+        result_5 = emotion_detector('I am really afraid that this will happen')
+        self.assertEqual(result_5['Dominant'], 'fear')
+
 unittest.main()
-
